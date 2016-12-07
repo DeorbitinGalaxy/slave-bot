4e6f27c25c172b26dd991a4d153572f8f94ea1c7
Florian Knop <florian_knop@hotmail.com>
2016-12-07 13:14:04 +0100

    fix sendFile

--- plugins/commands/index.ts
+++ plugins/commands/index.ts
@@ -175,7 +175,8 @@ function commandList (bot: Client, db: Datastore, message: Message) {
 
 function sendBackup (bot: Client, db: Datastore, message: Message) {
    loadCommands(db, message.guild.id).then((list) => {
-     message.channel.sendFile(JSON.stringify(list, null, 4));
+     const buffer = new Buffer(JSON.stringify(list, null, 4), 'utf-8');
+     message.channel.sendFile(buffer, `cmd-backup-${message.guild.name}-${new Date()}`);
    });
 }
 
