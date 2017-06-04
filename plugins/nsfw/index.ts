66ec75017e9fa1f7ffa24e26a3617b6fe97fca0f
Florian Knop <florian_knop@hotmail.com>
2017-06-04 13:59:33 +0200

    nsfw plugin

---
+++ plugins/nsfw/index.ts
@@ -0,0 +1,33 @@
+import { Message, Emoji } from 'discord.js';
+
+import { SlaveBotPlugin, PluginConfiguration, split, Md, fromDiscordEvent } from '../../lib';
+
+export const plugin: SlaveBotPlugin = {
+  name: 'nsfw',
+  version: require('../../../package.json').version,
+  description: 'Add NSFW tag to message',
+  usage: '!nsfw {url}',
+  events: {
+    message (plugin: PluginConfiguration, message: Message) {
+
+      const parts: string[] = split(message);
+
+      if (parts[0] === '!nsfw') {
+
+        if (!parts[1]) {
+          return 
+        }
+
+        const url = parts.slice(1, parts.length);
+
+        return message.delete().then(() => {
+          return message.channel.sendMessage(`[NSFW] From ${message.author.toString} : <${url}>`)
+        })
+        .catch(() => {
+          return message.channel.sendMessage(`But Master ${message.author.toString()}, I can't delete messages :persevere:`);
+        });
+      }
+    }
+  }
+}
+
